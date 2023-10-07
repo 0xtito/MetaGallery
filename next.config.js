f73043f5027fd3322af0a042079b0f0954d3aff9
0xtito <welovethepenguins@gmail.com>
2023-10-06 21:29:56 -0400

    added libraries + glsl support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.glsl/,
+      loader: "webpack-glsl-loader",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
