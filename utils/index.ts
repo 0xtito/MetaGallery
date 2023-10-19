86c60e1c365acb22a3d3349a7de46120454f6b4c
0xtito <welovethepenguins@gmail.com>
2023-10-19 15:42:58 -0400

    updated types + utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { nfts, chunkedNfts } from "@/utils/types";
 
 export function wrapText(text: string, maxLength: number): string {
   const words = text.split(" ");
@@ -20,4 +21,24 @@ export function wrapText(text: string, maxLength: number): string {
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function chunkArray(array: nfts[], chunkSize: number): chunkedNfts[] {
+  const results: chunkedNfts[] = [];
+  while (array.length) {
+    if (array.length < chunkSize) {
+      const chunk = array.splice(0, array.length);
+      results.push({
+        nfts: chunk,
+        id: chunk.reduce((acc, { id }) => (acc += id), ""),
+      });
+    } else {
+      const chunk = array.splice(0, chunkSize);
+      results.push({
+        nfts: chunk,
+        id: chunk.reduce((acc, { id }) => (acc += `${id}-`), ""),
+      });
+    }
+  }
+  return results;
 }
--- utils/types.ts
+++ utils/types.ts
@@ -1,5 +1,8 @@
 import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
 import type { XCameraRayIntersection } from "@coconut-xr/xinteraction";
+import type { ThreeEvent } from "@react-three/fiber";
+import type { Vector3 } from "three";
+import type { ButtonState } from "@coconut-xr/natuerlich/react";
 
 export type PlaneTypes =
   | "desk"
@@ -37,13 +40,44 @@ export type onIntersectionCallback =
 export interface SpacialPlaneProps {
   plane: XRPlane;
   name: string;
-  orientation?: "horizontal" | "vertical";
   mass?: number;
   color?: string;
 }
 
 export interface SpacialBoxProps {
   mesh: XRMesh;
+  name: string;
   color?: string;
   mass?: number;
 }
+export interface GrabProps {
+  children: React.ReactNode;
+  handleGrab: (e: ThreeEvent<PointerEvent>) => void;
+  handleRelease: (e: ThreeEvent<PointerEvent>, velocity?: Vector3) => void;
+  name?: string;
+}
+
+export interface nfts {
+  title: string;
+  url: string;
+  id: string;
+  additionalInfo?: string;
+}
+
+export interface chunkedNfts {
+  nfts: nfts[];
+  id: string;
+}
+
+export type ButtonsType = "a-button" | "b-button" | "x-button" | "y-button";
+
+export interface ControllerReaderState {
+  thumbstickX: number;
+  thumbstickY: number;
+  thumbstickState: ButtonState | undefined;
+  buttonsState: ButtonsObjType;
+}
+
+export type ButtonsObjType = Record<ButtonsType, ButtonState>;
+
+export type ControllerReaderReturn = [ControllerReaderState, XRHandedness];
