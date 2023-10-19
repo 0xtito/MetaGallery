8d9da4b49672d61c22e9aca35a1606fbb2eda0d3
0xtito <welovethepenguins@gmail.com>
2023-10-19 15:45:01 -0400

    clean xr/page.tsx

--- app/xr/page.tsx
+++ app/xr/page.tsx
@@ -3,7 +3,6 @@ import WebXRInit from "@/components/client/xr/WebXRInit";
 export default function XRPage() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
-      {/* <Scene /> */}
       <WebXRInit />
     </div>
   );
