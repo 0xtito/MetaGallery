c615c9fb108887c1c73dd0bc6e249e9f7855edb8
0xtito <welovethepenguins@gmail.com>
2023-10-19 15:40:18 -0400

    init api

---
+++ app/api/users/route.ts
@@ -0,0 +1,9 @@
+/**
+ * Here we define the routes for the users API
+ * GET: used to get the current active user and their nfts
+ * POST: used to set the active user and/or create a new user
+ *
+ * We will need to store
+ * - their Web3Auth information
+ * - their linked account
+ */
