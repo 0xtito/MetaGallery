d6302a9a85ebda38be25dbc3f919732d2923439d
0xtito <welovethepenguins@gmail.com>
2023-10-19 20:20:08 -0400

    web3 int

--- app/xr/page.tsx
+++ app/xr/page.tsx
@@ -1,6 +1,8 @@
 import Web3AuthProvider from "@/components/client/providers/Web3AuthProvider";
 import XRLoginPage from "@/components/client/XRLoginPage";
 
+// web3 integration
+
 export default function XRPage() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
