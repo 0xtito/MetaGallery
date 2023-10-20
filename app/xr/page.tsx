28e9a14e8a48c860e1fb7e47d5b513449ca2c090
0xtito <welovethepenguins@gmail.com>
2023-10-20 17:02:30 -0400

    hide SafeAuth while testing

--- app/xr/page.tsx
+++ app/xr/page.tsx
@@ -1,12 +1,14 @@
-import Web3AuthProvider from "@/components/client/providers/Web3AuthProvider";
-import XRLoginPage from "@/components/client/XRLoginPage";
+// import Web3AuthProvider from "@/components/client/providers/SafeAuthProvider";
+// import XRLoginPage from "@/components/client/XRLoginPage";
+import WebXRInit from "@/components/client/xr/WebXRInit";
 
 export default function XRPage() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
-      <Web3AuthProvider>
+      {/* <Web3AuthProvider>
         <XRLoginPage />
-      </Web3AuthProvider>
+      </Web3AuthProvider> */}
+      <WebXRInit />
     </div>
   );
 }
